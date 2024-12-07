978ee6f692dcb04c33b7c057ccca48d8b6c9fb39
mhmetkartl <kartalmehmetbedirhan@gmail.com>
2024-12-07 10:59:00 +0300

    Create arac_on_radar.ino

---
+++ arac_on_radar/arac_on_radar.ino
@@ -0,0 +1,62 @@
+int trig_left = 8;
+int trig_right = 10;
+
+int echo_left = 9;
+int echo_right = 11;
+
+long time_left;
+long time_right;
+
+long distance_left;
+long distance_right;
+
+int buzzer 12;
+
+
+void setup() {
+
+  pinMode(trig_left, OUTPUT);
+  //pinMode(trig_right, OUTPUT);
+  pinMode(echo_left, INPUT);
+  pinMode(buzzer,OUTPUT);
+  //pinMode(echo_right, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trig_left, 0);
+  delayMicroseconds(5);
+  digitalWrite(trig_left, 1);
+  delayMicroseconds(10);
+  digitalWrite(trig_left, 0);
+
+  time_left = pulseIn(echo_left, 1);
+  distance_left = time_left / 29.1 / 2;
+
+
+ /* digitalWrite(trig_right, 0);
+  delayMicroseconds(5);
+  digitalWrite(trig_right, 1);
+  delayMicroseconds(10);
+  digitalWrite(trig_right, 0);
+
+  time_right = pulseIn(echo_right, 1);
+  distance_right = time_right / 29.1 / 2; */
+
+  Serial.print("left: ");
+  Serial.println(distance_left);
+  //Serial.print("   ");
+  //Serial.print("right: ");
+  //Serial.println(distance_right);
+
+  if(distance_left < 25);
+  {
+    digitalWrite(buzzer,1);
+
+  }
+  else
+  {
+    digitalWrite(buzzer,0);
+  }
+}
